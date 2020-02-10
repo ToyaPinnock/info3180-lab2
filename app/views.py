a7b11b5d0adbd0efe1a441e2d0387dbbf2647365
toyapinnock <toyapinnock@gmail.com>
2020-02-09 23:15:31 -0500

    updates

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -12,6 +12,12 @@ from flask import render_template, request, redirect, url_for, flash
 ###
 # Routing for your application.
 ###
+@app.route('/profile')
+def profile():
+
+    return render_template('profile.html', date="feb,2 202")
+
+
 
 @app.route('/')
 def home():
@@ -52,6 +58,12 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+def format_date_joined(date):
+    date_joined= datetime.date(date)
+    return date_joined.strftime("%B, %Y")
+
+
 
 
 if __name__ == '__main__':
